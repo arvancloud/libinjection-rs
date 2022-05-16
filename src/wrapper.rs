ff30d579d2fa7217160f38baa01485c7b26474cf
sahandevs <sahandevs@gmail.com>
2022-05-16 15:20:48 +0430

    fix buffer overflow in `sqli`'s fingerprint

--- src/wrapper.rs
+++ src/wrapper.rs
@@ -1,20 +1,16 @@
 use bindings;
-use std::ffi::CString;
+use std::ffi::{CStr, CString};
 
 /// Checks `input` for SQL injection detection, and returns an option of (is_sqli, fingerprint)
 pub fn sqli(input: &str) -> Option<(bool, String)> {
-    let fingerprint_cstring = CString::new("").ok()?;
-    let fingerprint_raw_ptr = fingerprint_cstring.into_raw();
+    let mut fingerprint = ['\0'; 8];
+    let fingerprint_ptr = fingerprint.as_mut_ptr() as *mut i8;
     let input_cstring = CString::new(input).ok()?;
     let input_ptr = input_cstring.as_ptr();
     let is_sqli =
-        unsafe { bindings::libinjection_sqli(input_ptr, input.len() as u64, fingerprint_raw_ptr) };
-    Some((
-        is_sqli == 1,
-        unsafe { CString::from_raw(fingerprint_raw_ptr) }
-            .into_string()
-            .ok()?,
-    ))
+        unsafe { bindings::libinjection_sqli(input_ptr, input.len() as u64, fingerprint_ptr) };
+    let fingerprint = unsafe { CStr::from_ptr(fingerprint_ptr).to_str().ok()?.to_string() };
+    Some((is_sqli == 1, fingerprint))
 }
 
 /// Checks `input` for XSS detection, and returns an option of is_xss
